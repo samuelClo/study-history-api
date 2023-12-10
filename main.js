c7198b21e29a62dd73f9acb8a8a52e48b6f9597d
samuelClo <45668903+samuelClo@users.noreply.github.com>
2023-12-10 23:52:08 +0100

    feat: use html anchor instead of complex javascript logic

--- main.js
+++ main.js
@@ -2,94 +2,36 @@
 
 const mainEl = document.querySelector("main");
 
-const isViewModeMosaic = () => mainEl.getAttribute("view-mode") === "mosaic";
-
-const scrollToArticle = (articleId) => {
-  const article = document.querySelector(
-    `article[data-article-id="${articleId}"]`
-  );
-
-  if (article) {
-    window.scrollTo({ top: article.offsetTop, behavior: "smooth" });
-  } else {
-    console.error(`Article with ID ${articleId} not found`);
-  }
-};
-
-/**
- * @description use to navigate in article page
- * - addViewList => create a new history entry corresponding to the view type list
- * - displayViewMosaic => display the view type mosaic
- * - displayViewList => display the view type list
- */
 const articlePageNavigation = {
-  addViewList: (articleId) => {
-    const newUrl = new URL(location.toString());
-    newUrl.searchParams.set("article-id", articleId);
-    history.pushState({ viewMode: "list", articleId }, "", newUrl);
-
-    mainEl.setAttribute("view-mode", "list");
-
-    scrollToArticle(articleId);
-  },
   displayViewMosaic: () => {
-    const newUrl = new URL(location.toString());
-    newUrl.searchParams.delete("article-id");
-
-    history.replaceState({ viewMode: "mosaic" }, "", newUrl);
-
     mainEl.setAttribute("view-mode", "mosaic");
   },
-  displayViewList: () => {
-    const newUrl = new URL(location.toString());
-    const articleId = history.state?.articleId
-      ? history.state.articleId
-      : newUrl.searchParams.get("article-id");
-
-    newUrl.searchParams.set("article-id", articleId);
-
-    history.replaceState(
-      {
-        viewMode: "list",
-        articleId: articleId,
-      },
-      "",
-      newUrl
-    );
-
+  displayViewList: (articleIdParam) => {
     mainEl.setAttribute("view-mode", "list");
-    scrollToArticle(articleId);
   },
-};
-
-const handleArticleClick = (article) => {
-  if (!isViewModeMosaic()) return;
-
-  const articleId = article.getAttribute("data-article-id");
-  articlePageNavigation.addViewList(articleId);
-};
-
-const setInitialViewMode = () => {
-  const currentUrl = new URL(location.toString());
-  const articleIdInURL = currentUrl.searchParams.get("article-id");
-
-  if (articleIdInURL && articleIdInURL.length > 0) {
-    articlePageNavigation.displayViewList();
-  } else {
-    articlePageNavigation.displayViewMosaic();
-  }
 };
 
 document.querySelectorAll("article").forEach((article) => {
-  article.addEventListener("click", () => handleArticleClick(article));
+  article.addEventListener("click", () => {
+    articlePageNavigation.displayViewList();
+  });
 });
 
-window.addEventListener("popstate", (e) => {
-  if (e.state.viewMode === "mosaic") {
-    articlePageNavigation.displayViewMosaic();
-  } else if (e.state.viewMode === "list") {
+document.querySelector("button").addEventListener("click", () => {
+  articlePageNavigation.displayViewMosaic();
+});
+
+// Check html anchor
+if (window.location.hash.substring()) {
+  articlePageNavigation.displayViewList();
+} else {
+  articlePageNavigation.displayViewMosaic();
+}
+
+window.addEventListener("popstate", () => {
+  if (window.location.hash.substring()) {
     articlePageNavigation.displayViewList();
+  } else {
+    articlePageNavigation.displayViewMosaic();
   }
 });
-
-setInitialViewMode();
